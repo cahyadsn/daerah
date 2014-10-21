9c647ba3383624bf5495c36afd4936f9e56dc2a4
cahyadsn <cahyadsn@gmail.com>
2014-10-21 16:34:20 +0700

    merapikan script

--- ajax_daerah.js
+++ ajax_daerah.js
@@ -1,68 +1,68 @@
 ﻿var ajaxku=buatajax();
 function ajaxkota(id){
-    var url="select_daerah.php?prop="+id+"&sid="+Math.random();
-    ajaxku.onreadystatechange=stateChanged;
-    ajaxku.open("GET",url,true);
-    ajaxku.send(null);
+  var url="select_daerah.php?prop="+id+"&sid="+Math.random();
+  ajaxku.onreadystatechange=stateChanged;
+  ajaxku.open("GET",url,true);
+  ajaxku.send(null);
 }
 
 function ajaxkec(id){
-    var url="select_daerah.php?kab="+id+"&sid="+Math.random();
-    ajaxku.onreadystatechange=stateChangedKec;
-    ajaxku.open("GET",url,true);
-    ajaxku.send(null);
+  var url="select_daerah.php?kab="+id+"&sid="+Math.random();
+  ajaxku.onreadystatechange=stateChangedKec;
+  ajaxku.open("GET",url,true);
+  ajaxku.send(null);
 }
 
 function ajaxkel(id){
-    var url="select_daerah.php?kec="+id+"&sid="+Math.random();
-    ajaxku.onreadystatechange=stateChangedKel;
-    ajaxku.open("GET",url,true);
-    ajaxku.send(null);
+  var url="select_daerah.php?kec="+id+"&sid="+Math.random();
+  ajaxku.onreadystatechange=stateChangedKel;
+  ajaxku.open("GET",url,true);
+  ajaxku.send(null);
 }
 
 function buatajax(){
-    if (window.XMLHttpRequest){
-      return new XMLHttpRequest();
-    }
-    if (window.ActiveXObject){
-      return new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    return null;
+  if (window.XMLHttpRequest){
+    return new XMLHttpRequest();
+  }
+  if (window.ActiveXObject){
+    return new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  return null;
 }
 function stateChanged(){
-    var data;
-    if (ajaxku.readyState==4){
-      data=ajaxku.responseText;
+  var data;
+  if (ajaxku.readyState==4){
+    data=ajaxku.responseText;
     if(data.length>=0){
       document.getElementById("kota").innerHTML = data
     }else{
       document.getElementById("kota").value = "<option selected>Pilih Kota/Kab</option>";
     }
-    }
+  }
 }
 
 function stateChangedKec(){
-    var data;
-    if (ajaxku.readyState==4){
-      data=ajaxku.responseText;
+  var data;
+  if (ajaxku.readyState==4){
+    data=ajaxku.responseText;
     if(data.length>=0){
       document.getElementById("kec").innerHTML = data
     }else{
       document.getElementById("kec").value = "<option selected>Pilih Kecamatan</option>";
     }
-    }
+  }
 }
 
 function stateChangedKel(){
-    var data;
-    if (ajaxku.readyState==4){
+  var data;
+  if (ajaxku.readyState==4){
     data=ajaxku.responseText;
     if(data.length>=0){
-    document.getElementById("kel").innerHTML = data
+      document.getElementById("kel").innerHTML = data
     }else{
-    document.getElementById("kel").value = "<option selected>Pilih Kelurahan/Desa</option>";
+      document.getElementById("kel").value = "<option selected>Pilih Kelurahan/Desa</option>";
     }
-    }
+  }
 }
 
 function showCoordinate(){
@@ -87,5 +87,5 @@ function showCoordinate(){
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
-  });;
+  });
 }
--- index.php
+++ index.php
@@ -1,66 +1,66 @@
 ﻿<!doctype html>
 <html>
-	<head>
-		<title>Data Daerah</title>
-		<script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+  <head>
+    <title>Data Daerah</title>
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
     <script type="text/javascript" src="ajax_daerah.js"></script>
-	</head>
-	<body>
-		<table>
-			<tr>
-			<td>Pilih Provinsi</td>
-			<td>:</td>
-			<td>
-				<select name="prop" id="prop" onchange="ajaxkota(this.value)">
-					<option value="">Pilih Provinsi</option>
-					<?php 
-					include 'koneksi.php';
-					$query=$db->prepare("SELECT id_prov,nama FROM provinsi ORDER BY nama");
-					$query->execute();
-					while ($data=$query->fetchObject()){
-					echo '<option value="'.$data->id_prov.'">'.$data->nama.'</option>';
-					}
-					?>
-				<select>
-			</td>
-		</tr>
-		<tr>
-			<td>Pilih Kota/Kab</td>
-			<td>:</td>
-			<td>
-				<select name="kota" id="kota" onchange="ajaxkec(this.value)">
-					<option value="">Pilih Kota</option>
-				</select>
-			</td>
-		</tr>
-		<tr>
-			<td>Pilih Kec</td>
-			<td>:</td>
-			<td>
-				<select name="kec" id="kec" onchange="ajaxkel(this.value)">
-					<option value="">Pilih Kecamatan</option>
-				</select>
-			</td>
-		</tr>
-		<tr>
-			<td>Pilih Kelurahan/Desa</td>
-			<td>:</td>
-			<td>
-				<select name="kel" id="kel" onchange="showCoordinate();">
-					<option value="">Pilih Kelurahan/Desa</option>
-				</select>
-			</td>
-		</tr>
-		<tr>
-		  <td>Latitude</td>
-		  <td>:</td>
-		  <td><input type='text' id='lat' readonly></td>
-		</tr>
-		<tr>
-		  <td>Longitude</td>
-		  <td>:</td>
-		  <td><input type='text' id='lng' readonly></td>
-		</tr>
-		</table>
-	</body>
+  </head>
+  <body>
+    <table>
+      <tr>
+      <td>Pilih Provinsi</td>
+      <td>:</td>
+      <td>
+        <select name="prop" id="prop" onchange="ajaxkota(this.value)">
+          <option value="">Pilih Provinsi</option>
+          <?php 
+          include 'koneksi.php';
+          $query=$db->prepare("SELECT id_prov,nama FROM provinsi ORDER BY nama");
+          $query->execute();
+          while ($data=$query->fetchObject()){
+          echo '<option value="'.$data->id_prov.'">'.$data->nama.'</option>';
+          }
+          ?>
+        <select>
+      </td>
+    </tr>
+    <tr>
+      <td>Pilih Kota/Kab</td>
+      <td>:</td>
+      <td>
+        <select name="kota" id="kota" onchange="ajaxkec(this.value)">
+          <option value="">Pilih Kota</option>
+        </select>
+      </td>
+    </tr>
+    <tr>
+      <td>Pilih Kec</td>
+      <td>:</td>
+      <td>
+        <select name="kec" id="kec" onchange="ajaxkel(this.value)">
+          <option value="">Pilih Kecamatan</option>
+        </select>
+      </td>
+    </tr>
+    <tr>
+      <td>Pilih Kelurahan/Desa</td>
+      <td>:</td>
+      <td>
+        <select name="kel" id="kel" onchange="showCoordinate();">
+          <option value="">Pilih Kelurahan/Desa</option>
+        </select>
+      </td>
+    </tr>
+    <tr>
+      <td>Latitude</td>
+      <td>:</td>
+      <td><input type='text' id='lat' readonly></td>
+    </tr>
+    <tr>
+      <td>Longitude</td>
+      <td>:</td>
+      <td><input type='text' id='lng' readonly></td>
+    </tr>
+    </table>
+  </body>
 </html>
